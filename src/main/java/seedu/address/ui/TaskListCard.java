a65176cba77fb629961395d482d28d90d3453e76
yeojunjie <54196980+yeojunjie@users.noreply.github.com>
2022-10-13 08:46:17 +0800

    Change incorrect references to 'Person' in comments of Task-related UI classes

--- src/main/java/seedu/address/ui/TaskListCard.java
+++ src/main/java/seedu/address/ui/TaskListCard.java
@@ -7,19 +7,11 @@ import javafx.scene.layout.Region;
 import seedu.address.model.task.Task;
 
 /**
- * An UI component that displays information of a {@code Person}.
+ * An UI component that displays information of a {@code Task}.
  */
 public class TaskListCard extends UiPart<Region> {
 
     private static final String FXML = "TaskListCard.fxml";
-
-    /**
-     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
-     * As a consequence, UI elements' variable names cannot be set to such keywords
-     * or an exception will be thrown by JavaFX during runtime.
-     *
-     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
-     */
 
     public final Task task;
 
