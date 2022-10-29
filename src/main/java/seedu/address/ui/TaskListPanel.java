a382c21f34d5c1cef6a8e0cfa39be2577b1dfdcf
yeojunjie <54196980+yeojunjie@users.noreply.github.com>
2022-10-30 03:25:42 +0800

    Update Javadoc comments

--- src/main/java/seedu/address/ui/TaskListPanel.java
+++ src/main/java/seedu/address/ui/TaskListPanel.java
@@ -15,7 +15,7 @@ import seedu.address.model.grade.GradeKey;
 import seedu.address.model.task.Task;
 
 /**
- * Panel containing the list of persons.
+ * Panel containing the list of tasks.
  */
 public class TaskListPanel extends UiPart<Region> {
     private static final String FXML = "TaskListPanel.fxml";
@@ -42,7 +42,7 @@ public class TaskListPanel extends UiPart<Region> {
     }
 
     /**
-     * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
+     * Custom {@code ListCell} that displays the graphics of a {@code Task} using a {@code TaskListCard}.
      */
     static class TaskListViewCell extends ListCell<Task> {
         @Override
